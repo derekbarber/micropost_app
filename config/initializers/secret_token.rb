eb527518167890930d14ef92e27ad2d64ce2f465
Derek Barber <db@derekbarber.ca>
2012-02-15 15:08:01 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicropostApp::Application.config.secret_token = 'cb5843794172cfdb799f952f49a43417a2bd596ed62d066b10df572ede4a8997fc235799b37fe6ca656bf20b115074bb73eca243627fde29df6afd2abdb295d9'
