c14327293a0d53ff49d2461fc5b6ae6e993b725d
Derek Barber <db@derekbarber.ca>
2012-02-18 07:47:07 -0800

    Add link verification tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   
+  def logo
+    logo = image_tag("logo.png", alt: "Micropost App", class: "round")
+  end
+  
   # Returns the full title on a per-page basis
   def full_title(page_title)
     base_title = "Micropost App"
