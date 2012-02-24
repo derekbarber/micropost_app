fd540d78f3444a213beea76998f352fdeaaabc32
Derek Barber <db@derekbarber.ca>
2012-02-24 11:38:44 -0800

    Minor updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -17,5 +17,5 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
 end
