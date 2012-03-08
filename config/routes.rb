b7361cd9d77881b0b24307d0d969f07c3d6cfe60
Derek Barber <db@derekbarber.ca>
2012-03-08 12:21:02 -0800

    Finish user edit, update, index and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ MicropostApp::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    name      "Derek Barber"
-    email     "db@derekbarber.ca"
+    sequence(:name)   { |n| "Person #{n}" }
+    sequence(:email)  { |n| "person_#{n}@example.com"}
     password  "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
